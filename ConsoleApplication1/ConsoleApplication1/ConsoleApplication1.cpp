9b0026da63e425d811bc0ce99bd4f3eddeb2e6b4
LordLadybug <thomasracer@gmail.com>
2019-03-13 09:25:55 -0700

    Add broken code to find first character in string1 that matches the first character in string2 after an astericks. Split into two functions: one for finding characters after an astericks, and the second is the same substring comparer that will now only iterate one character at a time unless it encounters an astericks in string2, at which point it will hand off the string searching to the first function to grab the next character after an astericks.

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,7 +5,10 @@
 #include <iostream>
 #include <string>
 #include <stdexcept>
+#include <iterator>
+#include <tuple>
 
+std::tuple<std::string::iterator, std::string::iterator> FirstCharFinder(std::string string1, std::string string2);
 bool CompareForSubstrings(std::string string1, std::string string2);
 
 int main(int argc, char *argv[])
@@ -24,6 +27,7 @@ int main(int argc, char *argv[])
 			return -1;
 		}
 	}
+
 	else
 	{
 		//execute the rest of the program here
@@ -35,10 +39,47 @@ int main(int argc, char *argv[])
 	}
 }
 
+std::tuple<std::string::iterator, std::string::iterator> FirstCharFinder(std::string string1, std::string string2)	//after an astericks, finds first matching character in two strings
+{
+	std::string::iterator String2Grabber = string2.begin();
+	char& firstCharacter = string2.front();
+	if (firstCharacter == '*')
+	{
+		firstCharacter = String2Grabber._Getpnext(); //if string2 begins with wildcard, grab the second character in string2 instead
+	}
+	std::string::iterator String1Searcher = string1.begin(); //initialize to start of string 1
+	do
+	{
+		if (&String1Searcher == firstCharacter)
+		{
+			//Set to next character in each string, then continue with search outside of this while loop
+			String2Grabber++;
+			String1Searcher++;
+		}
+	} while (&String1Searcher != firstCharacter && String1Searcher != string1.begin());//Stops either upon finding a matching character to firstCharacter or the end of string1, whichever comes first
+}
+
 bool CompareForSubstrings(std::string string1, std::string string2) 
 {
 	//place implementation for checking if second string is a substring of the first
 	bool SubstringFlag = false; //return value for function; used to keep track of if string 2 is in fact a substring
+	
+	//grab first character of string2, then search string1 for same character
+	std::string::iterator String2Grabber = string2.begin();
+	char& firstCharacter = string2.front();
+	std::string::iterator String1Searcher = string1.begin(); //initialize to start of string 1
+	<String1Searcher, String2Grabber> = FirstCharFinder(std::string string1, std::string string2);
+	
+	//Proceed to search through next characters
+	while (&String1Searcher == &String2Grabber && String2Grabber != string2.end())
+	{
+		String2Grabber++;
+		String1Searcher++;
+		if (&String2Grabber == '*')
+		{
+			String1Searcher++;
+		}
+	}
 	return SubstringFlag;
 }
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
